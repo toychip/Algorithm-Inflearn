b9e2eb05306a43a502b234b2e51607690b2f03dd
toychip <dlawnsgud427@naver.com>
2023-07-01 22:49:18 +0900

    String(9)_ExtractionDigit

--- section1_string/S08_ValidPalindromeString.java
+++ section1_string/S08_ValidPalindromeString.java
@@ -7,15 +7,10 @@ public class S08_ValidPalindromeString {
     public String solution1(String str) {
         String answer = "YES";
 
-        String upperStr = str.toUpperCase().replaceAll("[^A-Z]", "");
-//        System.out.println("upperStr = " + upperStr);
+        String validStr = str.replaceAll("[^0-9]", "");
+        int toInt = Integer.parseInt(validStr);
 
-        String validStr = new StringBuilder(upperStr).reverse().toString();
-//        System.out.println("validStr = " + validStr);
-//
-        if(!validStr.equals(upperStr)){
-            answer = "NO";
-        }
+        answer = String.valueOf(toInt);
 
         return answer;
     }
---
+++ section1_string/S09_ExtractionDigit.java
@@ -0,0 +1,37 @@
+package section1_string;
+
+import java.util.Scanner;
+
+public class S09_ExtractionDigit {
+
+    public String solution1(String str) {
+        String answer = "YES";
+        char temp;
+
+        String excludeStr = str.replaceAll("[A-Za-z]", "");
+
+        char[] excludeChar = excludeStr.toCharArray();
+
+        for(int i = 1; i < excludeChar.length; i++){
+            for(int j = 0; j <= i; j++){
+                if(excludeChar[j] == '0'){
+                    temp = excludeChar[j];
+                    excludeChar[j] = excludeChar[i];
+                    excludeChar[i] = temp;
+                }
+            }
+        }
+
+        answer = new String(excludeChar);
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        S09_ExtractionDigit S8 = new S09_ExtractionDigit();
+        Scanner kb = new Scanner(System.in);
+
+        String next = kb.nextLine();
+        System.out.println(S8.solution1(next));
+    }
+}
