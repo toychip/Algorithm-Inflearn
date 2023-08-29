18bc464d4a8a05b6b58633bb7dfa99d3d2d3ea8f
toychip <dlawnsgud427@naver.com>
2023-08-29 20:47:23 +0900

    Array(12)_Mentoring

---
+++ section2_array/S12_Mentoring.java
@@ -0,0 +1,51 @@
+package section2_array;
+
+import java.util.*;
+
+class S12_Mentoring {
+
+    public int solution(int n, int m, int[][] arr){
+        int answer=0;
+
+        for(int i=1; i<=n; i++){
+
+            for(int j=1; j<=n; j++){
+                int cnt=0;
+
+                for(int k=0; k<m; k++){
+                    int pi=0, pj=0;
+
+                    for(int s=0; s<n; s++){
+
+                        if(arr[k][s]==i){
+                            pi=s;
+                        }
+                        if(arr[k][s]==j) {
+                            pj=s;
+                        }
+                    }
+                    if(pi<pj) cnt++;
+                }
+                if(cnt==m){
+                    answer++;
+                    System.out.println(i+" "+j);
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        S12_Mentoring S12 = new S12_Mentoring();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int m=kb.nextInt();
+        int[][] arr=new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j]=kb.nextInt();
+            }
+        }
+        System.out.print(S12.solution(n, m, arr));
+    }
+}
