a02a9ff49ccfbb9edba4d6b9123ef37188abbc6c
toychip <dlawnsgud427@naver.com>
2023-09-01 20:42:59 +0900

    TPSW(1)_CombiningTwoArrays

---
+++ section3_2pointers_slindingwindow/S01_CombiningTwoArrays.java
@@ -0,0 +1,42 @@
+package section3_2pointers_slindingwindow;
+
+import java.util.*;
+class S01_CombiningTwoArrays {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1=0, p2=0;
+        while(p1<n && p2<m){
+            if(a[p1]<b[p2]) answer.add(a[p1++]);
+            else answer.add(b[p2++]);
+        }
+        while(p1<n) answer.add(a[p1++]);
+        while(p2<m) answer.add(b[p2++]);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        S01_CombiningTwoArrays S01 = new S01_CombiningTwoArrays();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n=kb.nextInt();
+
+        int[] a=new int[n];
+
+        for(int i=0; i<n; i++){
+            a[i]=kb.nextInt();
+        }
+
+        int m=kb.nextInt();
+
+        int[] b = new int[m];
+
+        for(int i=0; i<m; i++){
+            b[i]=kb.nextInt();
+        }
+
+        for(int x : S01.solution(n, m, a, b)) {
+            System.out.print(x+" ");
+        }
+    }
+}
