2effc5fbc00076fdbf57ceda75ed750b10d8cbc6
toychip <dlawnsgud427@naver.com>
2023-09-02 11:32:34 +0900

    TPSW(2)_FindCommonArgs

---
+++ section3_2pointers_slindingwindow/S02_FindCommonArgs.java
@@ -0,0 +1,36 @@
+package section3_2pointers_slindingwindow;
+
+import java.util.*;
+class S02_FindCommonArgs {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int p1=0, p2=0;
+        while(p1<n && p2<m){
+            if(a[p1]==b[p2]){
+                answer.add(a[p1++]);
+                p2++;
+            }
+            else if(a[p1]<b[p2]) p1++;
+            else p2++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        S02_FindCommonArgs s02 = new S02_FindCommonArgs();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] a=new int[n];
+        for(int i=0; i<n; i++){
+            a[i]=kb.nextInt();
+        }
+        int m=kb.nextInt();
+        int[] b=new int[m];
+        for(int i=0; i<m; i++){
+            b[i]=kb.nextInt();
+        }
+        for(int s : s02.solution(n, m, a, b)) System.out.print(s+" ");
+    }
+}
